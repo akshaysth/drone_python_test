be4cc77d68e44e144b62035809de206a0b150ce8
Akshay Suthar <akshay.suthar@gmail.com>
2023-10-25 16:26:12 -0400

    Updating webhook URL

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-URL = "https://hooks.slack.com/services/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"
+URL = "https://hooks.slack.com/services/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"
 DATA = {"text": "Hello, World! This is Drone!"}
 
 
