46676ad7994dcc8162d61c6f0f6e8258eb4fbe70
Akshay Suthar <akshay.suthar@gmail.com>
2023-10-25 16:17:00 -0400

    Changing data type for post request

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 import requests
 
 URL = "https://hooks.slack.com/services/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"
-DATA = {"text": "Hello, World!"}
-HEADERS = {
-    "Content-Type": "application/json",
-}
+DATA = {"text": "Hello, World! This is Drone!"}
 
 
-requests.post(url=URL, headers=HEADERS, data=DATA)
+requests.post(url=URL, json=DATA)
 
 print('test')
