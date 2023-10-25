8a783b38efdd861082c2bd4a054efcbd5aa9c94f
Akshay Suthar <akshay.suthar@gmail.com>
2023-10-25 16:03:29 -0400

    Adding slack webhook test

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
- print('test')
+import requests
+
+URL = "https://hooks.slack.com/services/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"
+DATA = {"text": "Hello, World!"}
+HEADERS = {
+    "Content-Type": "application/json",
+}
+
+
+requests.post(url=URL, headers=HEADERS, body=DATA)
+
+print('test')
