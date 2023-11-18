213a4b0f236cdf8b6019b9769e0192bf80c2c1b9
Akshay Suthar <akshay.suthar@gmail.com>
2023-11-17 19:13:56 -0500

    updating code

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-import requests
+from hello import hello
 
-URL = "https://rocket.hyperios.space/hooks/65398b8e33c299ed89abc20c/chtNrvpjqx4vjQYL2KpcuTDr7RDHSu6W6SAJzdL9c7QAjwf8"
-DATA = {"text": "Hello, World! This is Drone!"}
-
-
-requests.post(url=URL, json=DATA)
-
-print('test')
+def test_hello():
+    assert hello() == "Hello, world!"
