0e54d11f323766a3de2ba68ae38a85b06caa3e45
Akshay Suthar <akshay.suthar@gmail.com>
2023-11-18 01:28:47 -0500

    drone test environment variable

--- app/hello.py
+++ app/hello.py
@@ -1,19 +1,12 @@
-import sentry_sdk
-
-sentry_sdk.init(
-    dsn="https://d09890daeb48aba9775c3057a28b5cb7@o4506243842572288.ingest.sentry.io/4506245309267968",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
-)
+import requests
 
 
 def hello():
     return "hello world!"
+
+
+def get_ip():
+    return requests.get("https://ifconfig.me").text
 
 
 if __name__ == "__main__":
